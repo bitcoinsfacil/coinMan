18994d36137cc2850e2005b6802b087be913e800
bitcoinsfacil <pedro@bitcoinsfacil.com>
2020-12-18 21:54:44 +0100

    commit 1, zero null, thanks to tipbot digitbyte

---
+++ bot.py
@@ -0,0 +1,136 @@
+import discord
+from tipper.tipper import *
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    #make sure bot doesnt reply to himself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/price'):
+        msg = getPriceMSG()
+        return await client.send_message(message.channel,msg)
+
+    if message.content.startswith('/help'):
+        helpmsg = "**/price** - shows the current price of Myce.\n**/deposit** or **/addr** - shows your deposit address.\n**/balance [0conf=false]** - shows your balance\n**/tip [user] [amount]** - tips another user coins.\n**/withdraw [amount] [address]** - withdraws funds to an external address.\n**/donate [amount]** - donation to myce community"
+        #return await client.send_message(message.channel,helpmsg)
+        await message.channel.send(helpmsg)
+
+    if message.content.startswith('/deposit') or message.content.startswith('/addr'):
+
+        account = message.author.id
+        print(type(account))
+        address = getAddress(account)
+
+        msg = '{0.author.mention}, your address is %s.'.format(message)%address
+        return await client.send_message(message.channel,msg)
+
+    if message.content.startswith('/balance 0conf'): #0conf balance
+        account = message.author.id
+        balance = getBalance(account,0)
+        msg = '{0.author.mention}, you have %f DGB, including 0-confirmations.'.format(message)%balance
+        return await client.send_message(message.channel,msg)
+
+    if message.content.startswith('/balance'):
+        account = message.author.id
+        print(account)
+        balance = getBalance(account)
+        #price = getPrice(float(balance))
+        msg = '{0.author.mention}, you have %f DGB'.format(message)%(balance)
+        return await client.send_message(message.channel,msg)
+
+    if message.content.startswith('/tip '):
+        tipper = message.author.id
+        content = message.content.split()[1:]
+        toTipMention = content[0]
+        toTip = toTipMention.replace('<@','').replace('>','') #remove <@> from ID
+        amount = content[1]
+
+        #catching errors
+        if not toTipMention[:2]=='<@':
+            return await client.send_message(message.channel,"{0.author.mention}, invalid account.".format(message))
+        try:
+            amount = float(amount)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, invalid amount.".format(message))
+
+        try:
+            tip(tipper,toTip,amount)
+            #price = getPrice(float(amount))
+            return await client.send_message(message.channel,"{0.author.mention} has tipped %s %f DGB.".format(message)%(toTipMention,amount))
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, insufficient balance.".format(message))
+
+    if message.content.startswith('/withdraw '):
+        account = message.author.id
+        amount = message.content.split()[1]
+        address = message.content.split()[2]
+
+        #catching errors again
+        if not validateAddress(address):
+            return await client.send_message(message.channel,"{0.author.mention}, invalid address.".format(message))
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, invalid amount.".format(message))
+
+        try:
+            txid = withdraw(account,address,amount)
+            return await client.send_message(message.channel,"{0.author.mention}, withdrawal complete, TXID %s".format(message)%txid)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, insufficient balance.".format(message))
+
+    if message.content.startswith('/rain '):
+        account = message.author.id
+        amount = float(message.content.split()[1])
+
+        if amount < 0.01:
+            return await client.send_message(message.channel,"{0.author.mention}, the amount must be bigger than 0.01 dgb.".format(message))
+        #catching errors again
+        try:
+            amount = float(amount)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, invalid amount.".format(message))
+
+        try:
+            eachtip = rain(account,amount) #the function returns each individual tip amount so this just makes it easier
+            return await client.send_message(message.channel,"{0.author.mention} has tipped %f ammo to everyone on this server!".format(message)%eachtip)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, insufficient balance.".format(message))
+
+
+    if message.content.startswith('/donate'):
+        account = message.author.id
+        address = "DRv2B21QDebJ96MEpzq6oCNkE1SY7msU2m"#DGB marketing donation account
+        amount = message.content.split()[1]
+
+        #catching errors
+        if not validateAddress(address):
+            return await client.send_message(message.channel,"{0.author.mention}, invalid address.".format(message))
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, invalid amount.".format(message))
+
+        try:
+            txid = withdraw(account,address,amount)
+            return await client.send_message(message.channel,"{0.author.mention}, Thank you for your donation! TXID: %s".format(message)%txid)
+        except ValueError:
+            return await client.send_message(message.channel,"{0.author.mention}, insufficient balance.".format(message))
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+
+client.run('Nzg4NDI0MjQxNDA2MTQ4NjA4.X9jTSw.iajrIO2pe-I049f9KkgocK10408')
---
+++ tipper/tipper.py
@@ -0,0 +1,83 @@
+import requests
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+#from flask_jsonrpc.proxy import ServiceProxy
+
+rpc_username = 'myceuser'
+rpc_password = 'mypass'
+rpc_ip = '127.0.0.1'
+rpc_port = 23511
+
+
+#Make rpc connection with DGB core
+def ServiceProxy():
+    return AuthServiceProxy('http://%s:%s@%s:%i'%(rpc_username, rpc_password, rpc_ip, rpc_port))
+
+rpc_connection = ServiceProxy()
+
+
+def validateAddress(address):
+    rpc_connection = ServiceProxy()
+    validate = rpc_connection.validateaddress(address)
+    return validate['isvalid']
+
+def getAddress(account):
+    rpc_connection = ServiceProxy()
+    account = rpc_connection.getaccountaddress(account)
+    print(account)
+
+    print(account)
+    return account
+
+def getBalance(account,minconf=1):
+    rpc_connection = ServiceProxy()
+    try:
+        balance = rpc_connection.getbalance(account,minconf)
+    except ValueError:
+        balance = -1
+    return balance
+
+def withdraw(account,destination,amount):
+    if amount > getBalance(account) or amount <= 0:
+        raise ValueError("Invalid amount.")
+    else:
+        rpc_connection = ServiceProxy()
+        return rpc_connection.sendfrom(account,destination,amount)
+
+def tip(account,destination,amount):
+    if amount > getBalance(account) or amount <= 0:
+        raise ValueError("Invalid amount.")
+    else:
+        rpc_connection = ServiceProxy()
+        rpc_connection.move(account,destination,amount)
+
+def rain(account,amount):
+    if amount > getBalance(account) or amount <= 0:
+        raise ValueError("Invalid amount.")
+    else:
+        rpc_connection = ServiceProxy()
+        accounts = rpc_connection.listaccounts()
+        amount = 0
+        eachTip = amount / len(accounts)
+        for ac in accounts:
+            tip(account,ac,eachTip)
+        return eachTip
+
+#API commands
+
+def getPrice():
+    api = requests.get('https://api.coinmarketcap.com/v1/ticker/digibyte/')
+    r = api.json()[0]
+    price = float(r['price_usd'])
+    price_btc = float(r['price_btc'])
+
+    return price, price_btc, r
+
+def getPriceMSG():
+
+    price, price_btc, r = getPrice()
+
+    msg = "1 DGB = %9.8lf BTC (%9.8lf USD)\nMarket Cap. = %12.2lf USD (Rank: %d)\nChange in USD: %4.2lf pt. (1H), %4.2lf pt. (24H), %4.2lf pt. (7D)" % \
+            (float(r['price_btc']), float(r['price_usd']), float(r['market_cap_usd']), int(r['rank']),\
+            float(r['percent_change_1h']), float(r['percent_change_24h']), float(r['percent_change_1h']))
+
+    return msg
